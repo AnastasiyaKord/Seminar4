09be1ea22d6cac0c44182faa00583714d3c0f684
Anastasiya <vipstop2303@mail.ru>
2023-05-19 15:23:42 +0300

    задача19ДЗ

---
+++ Задача19ДЗ/Program.cs
@@ -0,0 +1,16 @@
+﻿﻿// Задача c квадратами  Console.WriteLine(i * i);
+// или с кубами  Console.WriteLine(i * i * i);
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+
+// while(i <= number)
+// {
+//     Console.WriteLine(i * i);
+//     i++;
+// }
+for(int i = 1; i <= number; i++)
+{
+    Console.WriteLine(i * i * i);
+}
